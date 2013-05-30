f1c51a4849ffe2851404a4d6e076728c7b8bff6e
Matthew Stickney <fiendishlinux@gmail.com>
2013-05-29 22:39:29 -0400

    Convert class to generator

--- logging.py
+++ logging.py
@@ -1,28 +1,22 @@
 import heapq
 
-class JitteredSorter:
-    def __init__(self, max_jitter, iterable):
-        self.source = iterable
-        self.window = max_jitter
-        self.queue = []
+def jittered_sorter(max_jitter, iterable):
+    queue = []
 
-    def __iter__(self):
-        def time_window(queue):
-            max_time = heapq.nlargest(1, queue)
-            min_time = heapq.nsmallest(1, queue)
-            if len(max_time) == 0 or len(min_time) == 0:
-                return 0
-            return max_time[0] - min_time[0]
+    def time_window(queue):
+        max_time = heapq.nlargest(1, queue)
+        min_time = heapq.nsmallest(1, queue)
+        if len(queue) = 0:
+            return 0
+        return max_time[0] - min_time[0]
 
-        try:
-            while True:
-                while time_window(self.queue) <= self.window:
-                    heapq.heappush(self.queue, next(self.source))
-                yield heapq.heappop(self.queue)
-        except StopIteration:
-            # All the data's in, we're good to return an element
-            pass
-
-        while len(self.queue) > 0:
-            yield heapq.heappop(self.queue)
-
+    try:
+        while True:
+            while time_window(queue) <= max_jitter:
+                heapq.heappush(queue, next(iterable))
+            yield heapq.heappop(queue)
+    except StopIteration:
+        # No more data, set to yield the rest of the elements
+        pass
+    while len(queue) > 0:
+        yield heapq.heappop(queue)
