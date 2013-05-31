bbe9c8a28d2e530e9d60d91698aab37e6326d2e9
Matthew Stickney <fiendishlinux@gmail.com>
2013-05-31 08:59:53 -0400

    Add top-level event_stream() func and main()

--- logging.py
+++ logging.py
@@ -1,4 +1,5 @@
 import heapq
+import sys
 
 def jittered_sorter(max_jitter, iterable):
     queue = []
@@ -21,3 +22,15 @@ def jittered_sorter(max_jitter, iterable):
         pass
     while len(queue) > 0:
         yield heapq.heappop(queue)
+
+def event_stream(filename):
+    with open(filename) as fh:
+        lines = iter(fh)
+        splitlines = (l.split() for l in lines)
+        events = ((float(l[0]), ' '.join(l[1:])) for l in splitlines)
+        for evt in jittered_sorter(300, events):
+            yield evt
+
+if __name__ == "__main__":
+    for evt in event_stream(sys.argv[1]):
+        print "%f   %s" % (evt[0], evt[1])
