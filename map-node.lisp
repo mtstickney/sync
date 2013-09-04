b648dbbb62597b838b7e4e6a9457663945e8d630
Matthew Stickney <fiendishlinux@gmail.com>
2013-09-04 16:40:09 -0400

    Add some initial-cut ADD code for persistent-maps

--- map-node.lisp
+++ map-node.lisp
@@ -87,15 +87,57 @@
           (values nil nil)))))
 
 (defun max-partition (key partition-bits)
+  ;; We always require at least one partition
+  (max 1
+       (ceiling (integer-length key) partition-bits)))
 
-  )
+(defun reparent (root height hash partition-bits)
+  "Return a (possibly new) tree node that is tall enough to support KEY."
+  (let ((new-height (max-partition hash partition-bits))
+        (node-size (length (map-node-map root))))
+    (loop with node = root
+       for h from height to new-height
+       do (setf node (make-map-node node-size 0 node))
+       finally (return (values node (max height new-height))))))
 
-(defmethod add ((coll persistent-map) x &rest xs)
-  (let ((root (map-root coll))
-        (bits (map-node-bits coll))
-        (node-size (map-node-size coll))
-        (height (map-height coll))
-        )
+(defun insert-key (root height hash key val partition-bits &key copier)
+  (check-type root map-node)
+  (check-type hash fixnum)
+  (let ((new-root (funcall copier root))
+        (node-size (length (map-node-map root)))
+        (added-item nil))
+    (loop with node = new-root
+       for h from (1- height) downto 1
+       do (let* ((idx (key-partition hash h partition-bits))
+                 (new-child (if (has-item node idx)
+                                (funcall copier (element node idx))
+                                (make-map-node node-size))))
+            (setf (element node idx) new-child
+                  node new-child))
+       finally (let* ((idx (key-partition hash 0 partition-bits))
+                      (chain (element node idx)))
+                 (setf (element node idx)
+                       (cond
+                         ((not (has-item node idx))
+                          (setf added-item t)
+                          (acons key val nil))
+                         ((assoc key chain :test #'eql)
+                          (mapcar (lambda (c)
+                                    (if (eql (car c) key)
+                                        (cons key val)
+                                        c))
+                                  chain))
+                         (t
+                          (setf added-item t)
+                          (acons key val chain))))))
+    (values new-root added-item)))
+
+(defun add-item% (coll key val)
+  (let* ((root (map-root coll))
+         (bits (map-node-bits coll))
+         (node-size (map-node-size coll))
+         (height (map-height coll))
+         (hash (sxhash key)))
     ;; Note: after adding parents to extend the height of root to
     ;; (MAX-PARTITION key), Node copying needs to be done iff the
     ;; new root is EQ to the old root.
@@ -108,6 +150,27 @@
     ;; root, is thus in the same key space (subtree) as the nodes in
     ;; root, and thus nodes must be copied to avoid modification (the
     ;; root node, at minimum).
-    ()
-    )
-  )
+    (multiple-value-bind (new-root new-height) (reparent root height key bits)
+
+      (multiple-value-bind (new-root added-p)
+          (insert-key new-root new-height key val bits
+                      :copier (if (eq new-root root)
+                                  #'copy-map-node
+                                  #'identity))
+        (with-slots (root size height) coll
+          (setf root new-root
+                height new-height)
+          (when added-p
+            (incf size)))))
+    (values)))
+
+(defmethod add ((coll persistent-map) x &rest xs)
+  (assert (not (null xs)) (xs)
+          "ADD requires at least two arguments for a persistent-map.")
+  (let ((new-coll (copy-persistent-map coll)))
+    (add-item% new-coll x (car xs))
+    (loop for (key . rest) on xs by #'cddr
+       if (null rest)
+       do (error "ADD requires an even number of arguments.")
+       do (add-item% new-coll key (car rest)))
+    new-coll))
