37b9d66d35e58dac44f850de117a6a425463fb8e
Matthew Stickney <fiendishlinux@gmail.com>
2014-08-14 23:45:14 -0400

    Add implementation sketch for call().

--- shecl.c
+++ shecl.c
@@ -56,3 +56,37 @@ cl_object read(const char *s, cl_object pool)
                 ecl_return1(env, OBJNULL);
         } CL_CATCH_ALL_END
 }
+
+/* FIXME: no room for out-of-band error-handling param (!) */
+cl_object call(int nargs, cl_object func, cl_object arg, ...)
+{
+        cl_object apply;
+        cl_object error;
+        cl_object nreverse;
+        cl_object arglist = arg;
+        ecl_va_list varargs;
+        cl_env_ptr env = ecl_process_env();
+        int i;
+
+        CL_CATCH_ALL_BEGIN(env) {
+                error = ecl_make_symbol("ERROR", "CL");
+                ECL_HANDLER_CASE_BEGIN(env, ecl_list1(error)) {
+                        /* collect up a (lisp) list of arguments. */
+                        ecl_va_start(varargs, arg, nargs, 2);
+                        for (i = 0; i < nargs - 2; i++) {
+                                arglist = cl_cons(ecl_va_arg(arglist), arglist);
+                        }
+
+                        nreverse = ecl_make_symbol("NREVERSE", "CL");
+                        arglist = cl_funcall(2, nreverse, arglist);
+
+                        apply = ecl_make_symbol("APPLY", "CL");
+                        ecl_return1(env, cl_funcall(2, apply, arglist);
+                } ECL_HANDLER_CASE(1, condition) {
+                        /* FIXME: need to get the actual error info out somehow. */
+                        ecl_return1(env, OBJNULL);
+                } ECL_HANDLER_CASE_END
+        CL_CATCH_ALL_IF_CAUGHT {
+                ecl_return1(env, OBJNULL);
+        } CL_CATCH_ALL_END
+}
