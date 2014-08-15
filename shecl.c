0f00d845e0db11ca685061f466802673c026b29f
Matthew Stickney <mstickney@compassmax.com>
2014-08-15 15:17:56 -0400

    Reduce the scope of variables.

--- shecl.c
+++ shecl.c
@@ -44,17 +44,17 @@ cl_object read(const char *s, cl_object pool)
 /* FIXME: no room for out-of-band error-handling param (!) */
 cl_object call(int nargs, cl_object func, cl_object arg, ...)
 {
-        cl_object safe_apply;
-        cl_object error;
-        cl_object nreverse;
-        cl_object arglist = arg;
-        ecl_va_list varargs;
         cl_env_ptr env = ecl_process_env();
-        int i;
 
         CL_CATCH_ALL_BEGIN(env) {
-                error = ecl_make_symbol("ERROR", "CL");
+                cl_object error = ecl_make_symbol("ERROR", "CL");
                 ECL_HANDLER_CASE_BEGIN(env, ecl_list1(error)) {
+                        int i;
+                        ecl_va_list varargs;
+                        cl_object arglist = arg;
+                        cl_object nreverse = ecl_make_symbol("NREVERSE", "CL");
+                        cl_object safe_apply = ecl_make_symbol("SAFE-APPLY", "SHECL");
+
                         /* collect up a (lisp) list of arguments. */
                         ecl_va_start(varargs, arg, nargs, 2);
                         for (i = 0; i < nargs - 2; i++) {
@@ -62,10 +62,7 @@ cl_object call(int nargs, cl_object func, cl_object arg, ...)
                         }
                         ecl_va_end(varargs);
 
-                        nreverse = ecl_make_symbol("NREVERSE", "CL");
                         arglist = cl_funcall(2, nreverse, arglist);
-
-                        safe_apply = ecl_make_symbol("SAFE-APPLY", "SHECL");
                         return cl_funcall(2, safe_apply, arglist);
                 } ECL_HANDLER_CASE(1, condition) {
                         /* FIXME: need to get the actual error info out somehow. */
