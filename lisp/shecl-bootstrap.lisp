ce2752b6e94cc35ebcc59cc00037cd1f37514f5e
Matthew Stickney <mstickney@compassmax.com>
2014-08-20 18:52:39 -0400

    Handle errors inside BOOTSTRAP.

--- lisp/shecl-bootstrap.lisp
+++ lisp/shecl-bootstrap.lisp
@@ -5,8 +5,10 @@
 (in-package #:shecl-bootstrap)
 
 (defun bootstrap (load-path)
-  (let* ((load-pathname (parse-namestring load-path))
-         (load-directory (make-pathname :defaults load-pathname
-                                        :name nil :type nil)))
-    (load (merge-pathnames #P"asdf.fas" load-path))
-    (load (merge-pathnames #P"shecl.fasb" load-path))))
+  (handler-case
+      (let* ((load-pathname (parse-namestring load-path))
+             (load-directory (make-pathname :defaults load-pathname
+                                            :name nil :type nil)))
+        (load (merge-pathnames #P"asdf.fas" load-path))
+        (load (merge-pathnames #P"shecl.fasb" load-path)))
+    (t (c) (values c (write-to-string c :escape nil)))))
