ba4c4d22f679fe62d8953fe8b832d9017d72cde5
Matthew Stickney <fiendishlinux@gmail.com>
2014-10-29 18:05:39 -0400

    Add package exports.

--- package.lisp
+++ package.lisp
@@ -1,5 +1,42 @@
 ;;;; package.lisp
 
 (defpackage #:probuild
-  (:use #:cl #:annot.eval-when))
+  (:use #:cl #:annot.eval-when)
+  ;; ASDF components
+  (:export #:abl-system
+           #:abl-file
+           #:procedure-file
+           #:class-file
+           #:window-file
+           #:abl-module
+           #:abl-system)
 
+  ;; ASDF extension protocol
+  (:export #:component-databases
+           #:get-builder)
+
+  ;; Builder functions and protocols
+  (:export #:base-builder
+           #:server-builder
+           #:exec-builder
+           ;; Errors
+           #:build-error
+           #:build-failure
+           #:missing-source
+           #:build-server-exited
+           ;; The protocol
+           #:build-file
+           #:startedp
+           #:start-server
+           #:submit-job
+           #:shutdown-server
+           #:*progress-cmd*)
+
+  ;; Utilities
+  (:export #:output-directory
+           #:set-output-dir)
+
+  ;; Application bits
+  (:export #:invalid-args
+           #:invalid-option
+           #:main))
