7a1ad7b641bc39753b159af48c42352651bd1226
Matthew Stickney <fiendishlinux@gmail.com>
2014-10-28 21:36:01 -0400

    Add main application code.

--- probuild.lisp
+++ probuild.lisp
@@ -193,3 +193,118 @@
                             ,(merge-pathnames (merge-pathnames #P"**/*.*" path)))
                           :ignore-inherited-configuration
                           :disable-cache)))
+
+;;;; Main application stuff
+(defmethod asdf:perform :around ((op asdf:compile-op) (component abl-file))
+  (declare (special *print-status*))
+  (let ((printp (and (boundp '*print-status*) *print-status*))
+        (system (asdf:component-system component)))
+    (when printp
+      (format t "~&Compiling ~A..." (enough-namestring (asdf:component-pathname component)
+                                                       (asdf:component-pathname system))))
+    (call-next-method op component)
+    (when printp
+      (format t "done~%"))))
+
+(define-condition invalid-args (error)
+  ((reason :initarg :reason :accessor reason))
+  (:report (lambda (c s)
+             (format s "Invalid arguments: ~A" (reason c)))))
+(define-condition invalid-option (error)
+  ((opt :initarg :opt :accessor option))
+  (:report (lambda (c s)
+             (format s "'~A' is not a valid option." (option c)))))
+
+(defun process-argument (opt val)
+  (cond
+    ((equalp opt "--output-dir")
+     (set-output-dir (cl-fad:pathname-as-directory val)))
+    (t (error 'invalid-option :opt opt))))
+
+(defun process-args (args)
+  (when (null (second args))
+    (error 'invalid-args :reason "at least two arguments are required."))
+  (let ((opts (nthcdr 2 args)))
+    (loop for (opt . rest) on opts by #'cddr
+       when (null rest)
+       do (error 'invalid-args :reason (format nil "option '~A' is missing a value." opt))
+       do (process-argument opt (car rest)))
+    (list (first args) (second args))))
+
+(defun print-usage (&optional (stream *standard-output*))
+  (format stream "Usage: doozer <operation> <system> [--output-dir output]~%~%~
+~4T<operation> -- currently just 'compile'.
+~4T<system> -- the name of the system to operate on.
+~4Toutput -- the directory to use for output files."))
+
+(defun get-asdf-op (op)
+  (check-type op string)
+  (cond
+    ((equalp op "compile")
+     'asdf:compile-op)
+    (t (error "Unknown system operation ~S." op))))
+
+(defun system-present-p (system)
+  (handler-case
+      (asdf:find-system system)
+    (asdf:missing-component ()
+      (return-from system-present-p nil))))
+
+(defun shutdown-builders (system)
+  (check-type system asdf:system)
+  (labels ((shutdown-component (c)
+             (when (and (typep c 'abl-module)
+                        (builder c))
+               (shutdown-server (builder c)))
+             (when (typep c 'asdf:module)
+               (loop for child in (asdf:component-children c)
+                  do (shutdown-component child)))))
+    (shutdown-component system)))
+
+;; doozer <op> <system> --output-dir ../output/ --lib-dir foo/ --lib-dir
+;; blarg/baz/ --eval '(ql-setup:init-quicklisp)'
+(defun run-app (argv)
+  (let* (;; This is SBCL, so *d-p-d* will be the current directory
+         ;; (also (truename ".") has a bug)
+         ;; (*default-pathname-defaults* (truename ".")
+         (asdf:*central-registry* (cons *default-pathname-defaults* asdf:*central-registry*))
+         (*print-status* t)
+         (app-config:*base-directory* (cl-fad:pathname-directory-pathname (cl-fad:pathname-as-file (first argv))))
+         op
+         system)
+    (declare (special *print-status*))
+    ;; set a default output directory (will be overridden by
+    ;; process-args if --output-dir was supplied)
+    (set-output-dir *default-pathname-defaults*)
+    (handler-case
+        (let ((args (process-args (rest argv))))
+          (setf op (first args)
+                system (second args)))
+      ((invalid-args invalid-option) (c)
+        (format t "~A~%" c)
+       (print-usage)
+       (return-from run-app -1)))
+
+    (unless (system-present-p system)
+      (error "Couldn't find the system ~S (are you sure there's an ~A.asd file in this directory?)"
+             system
+             system))
+    (let ((system (asdf:find-system system))
+          (asdf-op (get-asdf-op op)))
+      (unwind-protect
+           (asdf:oos asdf-op system)
+        (shutdown-builders (asdf:find-system system))))
+    ;; Success error code
+    0))
+
+(defun main (argv)
+  (handler-case
+      (let ((code (run-app argv)))
+        (sb-ext:exit :code code))
+    (serious-condition (c)
+      ;; It's not paranoia if they're actually out to get you.
+      (handler-case
+          (progn (format t "Error: ~A" c)
+                 (sb-ext:exit :code -1))
+        (t ()
+          (sb-ext:exit :code -1))))))
