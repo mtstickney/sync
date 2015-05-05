27396d241404e9a4eaa4e59bbacf51a5ec1e3621
Matthew Stickney <fiendishlinux@gmail.com>
2015-05-05 01:02:48 -0400

    Add initial netstring implementation

---
+++ NetstringPlus/Netstring.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace NetstringPlus
+{
+    public class NetstringPlusAPI
+    {
+        public static void WriteNetstringBytes(System.IO.Stream stream, byte[] data)
+        {
+            byte[] bytes = NetstringBytes(data);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        public static byte[] ReadNetstring(System.IO.Stream stream)
+        {
+            NetstringDecoder decoder = new NetstringDecoder(stream);
+            return decoder.PumpMessage(); 
+        }
+
+        public static byte[] NetstringBytes(byte[] data)
+        {
+            String len = String.Format("{0:X}", data.Length);
+
+            byte[] headerBytes = System.Text.UTF8Encoding.UTF8.GetBytes(len);
+            byte[] sepBytes = System.Text.UTF8Encoding.UTF8.GetBytes(":");
+            byte[] termBytes = System.Text.UTF8Encoding.UTF8.GetBytes("\n");
+            byte[] result = new byte[headerBytes.Length + sepBytes.Length + data.Length + termBytes.Length];
+            int offset = 0;
+            System.Buffer.BlockCopy(headerBytes, 0, result, offset, headerBytes.Length);
+            offset += headerBytes.Length;
+            System.Buffer.BlockCopy(sepBytes, 0, result, offset, sepBytes.Length);
+            offset += sepBytes.Length;
+            System.Buffer.BlockCopy(data, 0, result, offset, data.Length);
+            offset += data.Length;
+            System.Buffer.BlockCopy(termBytes, 0, result, offset, termBytes.Length);
+            return result;
+        }
+
+        public static byte[] NetstringData(byte[] nstring)
+        {
+            return ReadNetstring(new System.IO.MemoryStream(nstring));
+        }
+
+        public static byte[] NetstringFromString(String str) {
+            byte[] data = System.Text.UTF8Encoding.UTF8.GetBytes(str);
+            return NetstringBytes(data);
+        }
+    }
+}
---
+++ NetstringPlus/NetstringDecoder.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetstringPlus
+{
+    public class NetstringDecoder
+    {
+        public enum DecoderState { INITIAL, HEADER, DATA, ENDOFDATA, COMPLETE };
+
+        public DecoderState state { get; set; }
+        public int dataSize { get; set; }
+        public byte[] data { get; set; }
+        private System.IO.Stream byteStream;
+        private System.IO.BinaryReader charStream;
+
+        public static NetstringDecoder NewDecoderForStream(System.IO.Stream stream) {
+            return new NetstringDecoder(stream);
+        }
+
+        public NetstringDecoder(System.IO.Stream stream) {
+            this.state = DecoderState.INITIAL;
+            this.byteStream = stream;
+            this.charStream = new System.IO.BinaryReader(stream, System.Text.Encoding.UTF8);
+        }
+
+        public static int HexDigit(char c) {
+            int i = "0123456789ABCDEFabcdef".IndexOf(c);
+            if (i > 15) {
+                return i - 6;
+            }
+            return i;
+        }
+
+        public void AddHeaderDigit(int digit) {
+            this.dataSize = this.dataSize * 16 + digit;
+        }
+
+        public void TransitionState(DecoderState toState) {
+            switch(toState) {
+                case DecoderState.INITIAL:
+                    // FIXME: throw error here
+                    break;
+                case DecoderState.HEADER:
+                    if (this.state != DecoderState.INITIAL) {
+                        // FIXME: throw error here
+                    }
+                    break;
+                case DecoderState.DATA:
+                    if (this.state != DecoderState.HEADER) {
+                        // FIXME: throw error here
+                    }
+                    break;
+                case DecoderState.ENDOFDATA:
+                    if (this.state != DecoderState.DATA && this.state != DecoderState.HEADER) {
+                        // FIXME: throw error here
+                    }
+                    break;
+                case DecoderState.COMPLETE:
+                    if (this.state != DecoderState.ENDOFDATA) {
+                        // FIXME: throw error here
+                    }
+                    break;
+            }
+            this.state = toState;
+        }
+
+        public void PumpElement() {
+            switch(this.state) {
+                case DecoderState.INITIAL:
+                case DecoderState.HEADER:
+                    {
+                        int c = this.charStream.ReadChar();
+                        if (c < 0)
+                        {
+                            throw new System.IO.EndOfStreamException();
+                        }
+
+                        int n = NetstringDecoder.HexDigit((char)c);
+                        if (c == ':')
+                        {
+                            if (this.state == DecoderState.INITIAL)
+                            {
+                                // FIXME: throw empty-header error here
+                            }
+                            else if (this.dataSize == 0)
+                            {
+                                this.TransitionState(DecoderState.ENDOFDATA);
+                            }
+                            else
+                            {
+                                this.data = new byte[this.dataSize];
+                                this.TransitionState(DecoderState.DATA);
+                            }
+                        }
+                        else if (n < 0)
+                        {
+                            // FIXME: throw invalid header character error
+                        }
+                        else
+                        {
+                            this.AddHeaderDigit(n);
+                            if (this.state == DecoderState.INITIAL)
+                            {
+                                this.TransitionState(DecoderState.HEADER);
+                            }
+                        }
+                        break;
+                    }
+                case DecoderState.DATA:
+                    {
+                        int count = this.byteStream.Read(this.data, 0, this.dataSize);
+                        if (count < this.dataSize)
+                        {
+                            String msg = String.Format("Only able to read {0} bytes of data (wanted {1})", count, this.dataSize);
+                            throw new System.IO.EndOfStreamException(msg);
+                        }
+                        this.TransitionState(DecoderState.ENDOFDATA);
+                        break;
+                    }
+                case DecoderState.ENDOFDATA:
+                    {
+                        int c = this.charStream.ReadChar();
+                        if (c < 0)
+                        {
+                            throw new System.IO.EndOfStreamException();
+                        }
+                        else if (c != '\n')
+                        {
+                            // FIXME: throw extra-data exception
+                        }
+                        else
+                        {
+                            this.TransitionState(DecoderState.COMPLETE);
+                        }
+                        break;
+                    }
+                case DecoderState.COMPLETE:
+                    // FIXME: throw error because we're already done
+                    break;
+            }
+        }
+
+        public byte[] PumpMessage() {
+            while (this.state != DecoderState.COMPLETE) {
+                this.PumpElement();
+            }
+            return this.data;
+        }
+
+        // TODO: Add a PumpMessages(count) method, or replace this one (count == -1 => pump all).
+        public List<byte[]> PumpAllMessages() {
+            List<byte[]> messages = new List<byte[]>();
+            while (true) {
+                try {
+                    byte[] message = this.PumpMessage();
+                    messages.Add(message);
+                } catch (System.IO.EndOfStreamException e) {
+                    return messages;
+                }
+            }
+        }
+
+        public void Resynchronize() {
+            // Attempt to resynchronize after error by discarding data until we read a newline.
+            while (true) {
+                int c = this.charStream.ReadChar();
+                if (c == '\n') {
+                    return;
+                }
+            }
+        }
+    }
+}
