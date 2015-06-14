d8e38d181c3f3ace2ffc2ed93982ce88e554982f
Matthew Stickney <fiendishlinux@gmail.com>
2015-06-14 19:35:30 -0400

    Add a class for remote errors.

---
+++ MtgDotNet/RemoteError.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MtgDotNet
+{
+    public class RemoteError : Exception
+    {
+        public string message;
+        public int code;
+        public dynamic data;
+
+        public RemoteError(string message, int code, dynamic data = null)
+        {
+            this.message = message;
+            this.code = code;
+            this.data = data;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Remote Error ({0}): {1}", this.code, this.message);
+        }
+    }
+}
