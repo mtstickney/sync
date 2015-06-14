2ceb9d8cc9a31c3a61a1824f38749ac1cde3e2c4
Matthew Stickney <fiendishlinux@gmail.com>
2015-06-14 19:34:09 -0400

    Add interface for rpc connections.

---
+++ MtgDotNet/IRPCConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MtgDotNet
+{
+    public interface IRPCConnection
+    {
+        Task Connect();
+        Task Disconnect();
+        Task<dynamic> InvokeRPCMethod(string service, string method, dynamic[] args, [Optional] bool isNotification);
+    }
+}
