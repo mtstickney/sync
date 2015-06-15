68be4166915062876829e9f011e6fcf6613398da
Matthew Stickney <fiendishlinux@gmail.com>
2015-06-15 15:26:44 -0400

    Add an exception class for unauthorized clients.

---
+++ MtgDotNet/ClientNotAuthorizedException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MtgDotNet
+{
+    public class ClientNotAuthorizedException : Exception
+    {
+        public byte[] key { get; set; }
+
+        public ClientNotAuthorizedException(byte[] key)
+        {
+            this.key = key;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Client with key {0} is not authorized", this.key);
+        }
+    }
+}
