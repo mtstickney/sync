16e6b379214b2eaea7d353d0f6bb3b9c037c6aeb
Matthew Stickney <mstickney@compassmax.com>
2015-06-17 18:07:31 -0400

    Add static factory methods for rpc connections.

--- MtgDotNet/MtgDotNet.cs
+++ MtgDotNet/MtgDotNet.cs
@@ -6,4 +6,38 @@ using System.Threading.Tasks;
 
 namespace MtgDotNet
 {
+    public abstract class MtgDotNetAPI
+    {
+        public static MtgDotNet.IRPCConnection MakeConnection(string host, int port)
+        {
+            MtgDotNet.Sys.IFramer framer = (MtgDotNet.Sys.IFramer)new MtgDotNet.Framers.NetstringPlusFramer();
+            MtgDotNet.Sys.ITransport transport = (MtgDotNet.Sys.ITransport)new MtgDotNet.Transports.AsynchronousTcpTransport(host, port);
+            MtgDotNet.IRPCConnection con = (MtgDotNet.IRPCConnection)new MtgDotNet.RPCConnection(framer, transport);
+            return con;
+        }
+
+        public static MtgDotNet.IRPCConnection MakeConnection(System.Net.Sockets.TcpClient socket)
+        {
+            MtgDotNet.Sys.IFramer framer = (MtgDotNet.Sys.IFramer)new MtgDotNet.Framers.NetstringPlusFramer();
+            MtgDotNet.Sys.ITransport transport = (MtgDotNet.Sys.ITransport)new MtgDotNet.Transports.AsynchronousTcpTransport(socket);
+            MtgDotNet.IRPCConnection con = (MtgDotNet.IRPCConnection)new MtgDotNet.RPCConnection(framer, transport);
+            return con;
+        }
+
+        public static MtgDotNet.IRPCConnection MakeEncryptedConnection(string host, int port, byte[] secret, byte[][] authorizedKeys)
+        {
+            MtgDotNet.Sys.IFramer framer = (MtgDotNet.Sys.IFramer)new MtgDotNet.Framers.NetstringPlusFramer();
+            MtgDotNet.Sys.ITransport transport = (MtgDotNet.Sys.ITransport)new MtgDotNet.Transports.AsynchronousTcpTransport(host, port);
+            MtgDotNet.IRPCConnection con = (MtgDotNet.IRPCConnection)new MtgDotNet.EncryptedRPCConnection(framer, transport, secret, authorizedKeys);
+            return con;
+        }
+
+        public static MtgDotNet.IRPCConnection MakeEncryptedConnection(System.Net.Sockets.TcpClient socket, byte[] secret, byte[][] authorizedKeys)
+        {
+            MtgDotNet.Sys.IFramer framer = (MtgDotNet.Sys.IFramer)new MtgDotNet.Framers.NetstringPlusFramer();
+            MtgDotNet.Sys.ITransport transport = (MtgDotNet.Sys.ITransport)new MtgDotNet.Transports.AsynchronousTcpTransport(socket);
+            MtgDotNet.IRPCConnection con = (MtgDotNet.IRPCConnection)new MtgDotNet.EncryptedRPCConnection(framer, transport, secret, authorizedKeys);
+            return con;
+        }
+    }
 }
