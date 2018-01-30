80e97837c20aeff33b0ab24968d77b8e663e77c5
Matthew Stickney <fiendishlinux@gmail.com>
2018-01-30 01:51:53 -0500

    Inherit from string type so .value works.

--- convert_shortcuts.py
+++ convert_shortcuts.py
@@ -193,7 +193,7 @@ class Variant (Structure):
     # here, but fuck that noise.
     _fields_ = [("variantData", VariantTagStruct)]
 
-class BStr(c_void_p):
+class BStr(c_wchar_p):
     def __init__(self, str):
         # A BStr is a 4-byte length header and a utf-16 string.
         # 4 bytes == 2 utf-16 characters, so we'll pad, then scribble
